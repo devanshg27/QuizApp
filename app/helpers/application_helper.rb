beabcd114f78234b14282314248c32f47652cf03
Devansh Gautam <devanshg27@gmail.com>
2017-09-18 03:47:14 +0530

    Fixed bug with client side rendering by making change in application.html.haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
       notice: 'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
   end
+  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    class_attr_index = html_tag.index 'class="'
+  
+    if class_attr_index
+      html_tag.insert class_attr_index+7, 'form-error '
+    else
+      html_tag.insert html_tag.index('>'), ' class="form-error"'
+    end
+  end
 end
